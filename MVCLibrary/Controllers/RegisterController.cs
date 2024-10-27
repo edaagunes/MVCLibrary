181f8001a16ec3b9fc6b94a83be129e6ff3224a7
edaagunes <edagunes00@hotmail.com>
2024-10-27 21:00:30 +0300

    Added Login and Register Forms and Completed Register iterations

---
+++ MVCLibrary/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLibrary.Models.Entity;
+
+namespace MVCLibrary.Controllers
+{
+    public class RegisterController : Controller
+    {
+        LibraryDbEntities context=new LibraryDbEntities();
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Member member)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+            context.Member.Add(member);
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
