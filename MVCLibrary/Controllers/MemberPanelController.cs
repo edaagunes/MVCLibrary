c0f3f7b67525977781efba58aec5e47ac883370e
edaagunes <edagunes00@hotmail.com>
2024-10-27 21:13:15 +0300

    Created MemberPanel

---
+++ MVCLibrary/Controllers/MemberPanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLibrary.Controllers
+{
+    public class MemberPanelController : Controller
+    {
+        // GET: MemberPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
