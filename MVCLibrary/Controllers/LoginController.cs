a73e13b62e112550576db13d65c63a481da86cd7
edaagunes <edagunes00@hotmail.com>
2024-10-28 17:33:57 +0300

    Added Login Iterations

--- MVCLibrary/Controllers/LoginController.cs
+++ MVCLibrary/Controllers/LoginController.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using MVCLibrary.Models.Entity;
 
 namespace MVCLibrary.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        LibraryDbEntities context=new LibraryDbEntities();
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Member member)
+        {
+            var values= context.Member.FirstOrDefault(x=>x.Mail==member.Mail && x.Password==member.Password);
+            if (values !=null)
+            {
+                FormsAuthentication.SetAuthCookie(values.Mail, false);
+                Session["Mail"]=values.Mail.ToString();
+              
+                return RedirectToAction("Index","MemberPanel");
+            }
+            else
+            {
+                return View();
+            }
+          
         }
     }
 }
--- MVCLibrary/Controllers/MemberPanelController.cs
+++ MVCLibrary/Controllers/MemberPanelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCLibrary.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,30 @@ namespace MVCLibrary.Controllers
 {
     public class MemberPanelController : Controller
     {
-        // GET: MemberPanel
+        LibraryDbEntities context = new LibraryDbEntities();
+       
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var memberMail = (string)Session["Mail"];
+            var values = context.Member.FirstOrDefault(x => x.Mail == memberMail);
+            return View(values);
         }
+        [HttpPost]
+        public ActionResult MemberProfile(Member p)
+        {
+            var value = (string)Session["Mail"];
+            var member = context.Member.FirstOrDefault(x => x.Mail == value);
+            member.Password = p.Password;
+            member.Name = p.Name;
+            member.Surname = p.Surname;
+            member.Photo = p.Photo;
+            member.Phone = p.Phone;
+            member.School = p.School;
+            member.Username = p.Username;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
