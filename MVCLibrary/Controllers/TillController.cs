9fb252ec627fa85dfab92681f3d6bb0d2a48a8f6
edaagunes <edagunes00@hotmail.com>
2024-10-30 16:26:21 +0300

    Added TillController

---
+++ MVCLibrary/Controllers/TillController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLibrary.Models.Entity;
+
+namespace MVCLibrary.Controllers
+{
+    public class TillController : Controller
+    {
+       LibraryDbEntities context=new LibraryDbEntities();
+        public ActionResult Index()
+        {
+            ViewBag.till=context.Penalty.Sum(x=>x.Money).ToString();
+            return View();
+        }
+    }
+}
